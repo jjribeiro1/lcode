47eae182defb65d3474c57581eb3ef2f67413af5
jjribeiro1 <jvsr.99@hotmail.com>
2025-09-21 13:32:46 -0300

    docs: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Packing API')
+    .setDescription('API for managing packing operations')
+    .setVersion('1.0')
+    .addTag('Packing')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
